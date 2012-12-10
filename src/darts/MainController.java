a22310c70752bdae2d2108826d70cb3a988d0307
Ryan Quinn <ryanwilliamquinn@gmail.com>
2012-12-09 22:34:26 -0800

    first commit - basic struts project setup

---
+++ src/darts/MainController.java
@@ -0,0 +1,12 @@
+package darts;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class MainController extends ActionSupport {
+
+    public String execute() throws Exception {
+		return SUCCESS;
+    }
+	
+
+}
